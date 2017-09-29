74c4a46ee8831c99bff720f4797c87d15d2d1fa7
Arham Jain <Ajusa@users.noreply.github.com>
2017-09-29 10:40:14 -0400

    gotta go fast

--- test.nim
+++ test.nim
@@ -2,14 +2,19 @@ import os
 import winim
 import strutils
 var frag = ""
+var lastString = ""
 var db = newSeq[string](0)
+var tabs = 0
 for str in readFile("db.txt").split:
   if len(str) > 3:
     db.add(str)
-proc complete(frag:string): string = 
+proc complete(frag:string, i: int): string = 
+  var counter = 0
   for str in db:
     if str.startsWith(frag):
-      return str.replace(frag, "")
+      if counter == i:
+        return str.replace(frag, "")
+      counter += 1
 proc save(i: int) =
   #var file = open("LOG.txt", fmAppend)
   let key = case i
@@ -39,15 +44,20 @@ proc main() =
     Sleep(50)
     for i in 8..190:
       if GetAsyncKeyState(cint(i)) == -32767:
-        if VK_SPACE == i:
+        if VK_SPACE == i or VK_RETURN == i:
           frag = ""
-        else: frag = frag & toLowerAscii($chr(i))
+          tabs = 0
+          lastString = ""
+        elif VK_BACK == i:
+          frag.delete(frag.len-2, frag.len-1)
+        elif chr(i) in Letters: frag = frag & toLowerAscii($chr(i))
         if VK_TAB == i:
           keybd_event(VK_BACK,0x9e.BYTE,0,0);
           keybd_event(VK_BACK,0x9e.BYTE,KEYEVENTF_KEYUP,0);
-          writeString(complete(frag.strip))
-
-
-#ip.u1.ki.dwFlags = KEYEVENTF_KEYUP; # KEYEVENTF_KEYUP for key release
-#SendInput(1, &ip, sizeof(INPUT));
+          for ch in lastString:
+            keybd_event(VK_BACK,0x9e.BYTE,0,0);
+            keybd_event(VK_BACK,0x9e.BYTE,KEYEVENTF_KEYUP,0);
+          lastString = complete(frag.strip, tabs)
+          writeString(lastString)
+          tabs += 1
 main()
